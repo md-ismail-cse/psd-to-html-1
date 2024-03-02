b50350aadda826f8f8a69a3fbb8106361a75bc0d
Md. Ismail <122370641+md-ismail-cse@users.noreply.github.com>
2024-03-02 22:42:14 +0600

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+//Bx slider
+$(document).ready(function(){
+    $('.slider-items').bxSlider({
+        auto: true
+    });
+});
+
+
+//Mean Menu
+$(document).ready(function () {
+	$('#main-menu').meanmenu({
+        meanMenuContainer: "#mobile-menu",
+        meanScreenWidth:"875",
+    });
+});
